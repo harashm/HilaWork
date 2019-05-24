893c10a38ca405e03fa18d84a8b310bb14a1f34c
harashm <micho1234@gmail.com>
2019-05-24 09:24:28 +0300

    First Commit

--- max_bipartite_matrching.py
+++ max_bipartite_matrching.py
@@ -1,23 +1,23 @@
 import pprint
+
 # Python program to find maximal Bipartite matching.
 
 AVAILABLE_DATES = [5, 6, 7, 8, 9, 10]
 NOTE = {'Hila': [6],
         'Yana': [5],
-        'Anat': [5,7,8],
-        'Lilach': [6,8],
+        'Anat': [5, 7, 8],
+        'Lilach': [6, 8],
         'Moran': [],
         'Meital': [10],
         }
 
+
 def create_graph():
     total_people = len(NOTE)
     total_available_dates = len(AVAILABLE_DATES)
-    graph = [[0 for i in range(total_people)] for j in range(total_available_dates)]
+    graph = [[0 for _ in range(total_people)] for _ in range(total_available_dates)]
 
-    date2idx = {}
-    for i, date in enumerate(AVAILABLE_DATES):
-        date2idx[AVAILABLE_DATES[i]] = i
+    date2idx = {key: i for (i, key) in enumerate(AVAILABLE_DATES)}
 
     for worker_idx, worker in enumerate(NOTE):
         # worker_idx = 0; worker = list(NOTE.keys())[0]
@@ -27,10 +27,13 @@ def create_graph():
             # print(f'worker_idx = {worker_idx}, date = {date}, date2idx[date] = {date2idx[date]}')
             graph[date2idx[date]][worker_idx] = 1
 
-    # pprint.pprint(graph)
+    pprint.pprint(graph)
     return graph
 
+
 bpGraph = create_graph()
+
+
 ##
 # bpGraph = [[0, 1, 1, 0, 0, 0],
 #            [1, 0, 0, 1, 0, 0],
@@ -48,31 +51,31 @@ class GFG:
         self.res = dict()
 
     # A DFS based recursive function that returns true if a matching for vertex u is possible
-    def bpm(self, u, matchR, seen):
+    def bpm(self, u, match_r, seen):
 
         # Try every job one by one
         for v in range(self.jobs):
 
             # If applicant u is interested in job v and v is not seen
-            if self.graph[u][v] and seen[v] == False:
+            if self.graph[u][v] and seen[v] is False:
 
                 # Mark v as visited
                 seen[v] = True
 
-                '''If job 'v' is not assigned to an applicant OR previously assigned  applicant for job v (which is
-                matchR[v]) has an alternate job available. Since v is marked as visited in the  above line,
-                matchR[v] in the following recursive call will not get job 'v' again'''
-                if matchR[v] == -1 or self.bpm(matchR[v], matchR, seen):
-                    matchR[v] = u
+                # If job 'v' is not assigned to an applicant OR previously assigned  applicant for job v (which is
+                # matchR[v]) has an alternate job available. Since v is marked as visited in the  above line,
+                # matchR[v] in the following recursive call will not get job 'v' again
+                if match_r[v] == -1 or self.bpm(match_r[v], match_r, seen):
+                    match_r[v] = u
                     self.res[v] = u
                     return True
         return False
 
     # Returns maximum number of matching
     def maxBPM(self):
-        '''An array to keep track of the applicants assigned to jobs. The value of matchR[i] is the applicant
-         number assigned to job i, the value -1 indicates nobody is assigned.'''
-        matchR = [-1] * self.jobs
+        """An array to keep track of the applicants assigned to jobs. The value of matchR[i] is the applicant
+         number assigned to job i, the value -1 indicates nobody is assigned."""
+        match_r = [-1] * self.jobs
 
         # Count of jobs assigned to applicants
         result = 0
@@ -81,10 +84,10 @@ class GFG:
             seen = [False] * self.jobs
 
             # Find if the applicant 'u' can get a job
-            if self.bpm(i, matchR, seen):
+            if self.bpm(i, match_r, seen):
                 result += 1
 
-        print(matchR)
+        print(match_r)
         return result
 
 
@@ -93,8 +96,8 @@ g = GFG(bpGraph)
 print("Maximum number of applicants that can get job is %d " % g.maxBPM())
 print(g.res)
 
-for key in g.res:
-    key_res = g.res[key]
-    print(f" Date: {AVAILABLE_DATES[key]}, who: {list(NOTE.keys())[key_res]}")
+for person in g.res:
+    key_res = g.res[person]
+    print(f" Date: {AVAILABLE_DATES[person]:3d},   who: {list(NOTE.keys())[key_res]}")
 
 # This code is contributed by Neelam Yadav
